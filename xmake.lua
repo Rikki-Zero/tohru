5a342d609cd3e3e0e2973d331dd5188adf47df1d
Rikki <rikki@member.fsf.org>
2024-12-20 01:23:06 +0800

    update: separate kobayashi-std into an independent dynamic library

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,16 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
-add_includedirs("src")
+add_includedirs("src","kobayashi-std")
+
+target("kobayashi-std")
+    set_kind("shared")
+    set_languages("c11")
+    set_optimize("fastest")
+    add_files("kobayashi-std/**.c")
 
 target("tohru")
     set_kind("binary")
     set_languages("c11")
     set_optimize("fastest")
-
     add_files("src/**.c")
